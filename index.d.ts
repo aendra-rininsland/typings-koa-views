f1b9d99db36a2f7323fdb66ba0866ae5991f5475
Ændrew Rininsland <andrew.rininsland@ft.com>
2016-08-22 16:47:58 +0100

    initial.

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+/**
+ * TypeScript definition for koa-views@next
+ * 2016 Ændrew Rininsland
+ */
+declare module 'koa-views' {
+  import * as Koa from 'koa';
+
+  function views(root: string, opts?: {
+    extension?: string,
+    map?: {
+      html: string;
+    }
+  }): { (ctx: ViewsContext, next?: () => any): any };
+
+  interface ViewsContext extends Koa.Context {
+    render(view: string, locals?: {extension: string}): Promise<string>;
+  }
+
+  export = views;
+}
---
+++ source-test/test.ts
@@ -0,0 +1,3 @@
+import test = require('blue-tape');
+
+import koaViews = require('koa-views');
